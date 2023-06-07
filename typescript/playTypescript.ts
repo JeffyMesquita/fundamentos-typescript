00785d19819ef5bdd559c428e606c23e8e2bee98
Jeferson Mesquita <je_2742@hotmail.com>
2023-06-07 19:08:50 -0300

    Tipos primitivos e Compile Options

--- typescript/playTypescript.ts
+++ typescript/playTypescript.ts
@@ -1,12 +1,21 @@
-// Tipagem estática
-function flipCoin() {
-  return Math.random() > 0.5 ? "Cara" : "Coroa";
-}
+const message: string = "Hello World";
+const numberValue: number = 10;
+const booleanValue: boolean = true;
 
-const result = flipCoin();
+const nullValue: null = null;
+const undefinedValue: undefined = undefined;
 
-if (result === "Cara") {
-  console.log("Você ganhou!");
-} else {
-  console.log("Você perdeu!");
-}
+const value: 'primary' | 'secondary' = 'primary';
+
+const list: string[] = ['a', 'b', 'c'];
+
+const users = [
+    { name: 'John', age: 30 },
+    { name: 'Jane', age: 25 },
+  ]
+
+// const john = users.find(user => user.name === 'John');
+// console.log(john?.age);
+
+const jane = users.length > 4 ? users[3] : null;
+console.log(jane?.age);
