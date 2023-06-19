14fb07aec330adbc2e89e5bf22d0473b6afc867a
Jeferson Mesquita <je_2742@hotmail.com>
2023-06-19 09:45:44 -0300

    Indexed Access Types

--- typescript/playTypescript.ts
+++ typescript/playTypescript.ts
@@ -1,5 +1,5 @@
 import {Post , PostComment, User, Company} from './interfaces';  
-import { vitor, post, comment, coffstack} from './mocksData';
+import { vitor, post, coffstack} from './mocksData';
 
 export const lucas: User = {
   name: "Lucas",
@@ -7,11 +7,17 @@ export const lucas: User = {
   email: "lucas@coffstacj.com",
 };
 
-function getUserProperty<Type>(user:Type, key: keyof Type){
-  return user[key];
+export const comment: PostComment = {
+  content: "My first comment",
+  likes: 5,
+  user: vitor, 
+};
+
+// type ContentType = PostComment['user']
+
+
+function getUserProperty<Type, Key extends keyof Type>(value:Type, key: Key): Type[Key]{
+  return value[key];
 }
 
-
 const value = getUserProperty(comment, 'content');
-
-console.log(value);
